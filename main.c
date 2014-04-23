465fc2352b4c8ec19d6fc9d64250bf8c7ed48de0
Graham Christensen <graham@grahamc.com>
2014-04-22 19:14:23 -0500

    add chores

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <string.h>
 #include <avr/io.h>
 #include <util/delay.h>
+
 #include "usart.h"
 #include "buttons.h"
+#include "chores.h"
 
 #define P_SPEAKER PD5
 
 int main (void) {
   USART0Init();
   buttons_init();
-
-
 
   while (1) {
     if (timer_state()) {
